038d683c314add564c90967f02a04c8c3422844d
tanxiang <1571922819@qq.com>
2023-12-21 15:07:54 +0800

    v1.0

--- upload.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-
-
-scp -r .\packages\web-client\dist\* root@119.29.246.22:/usr/software/client/nginx/html
-scp .\packages\web-client\nginx.conf root@119.29.246.22:/usr/software/client/nginx/conf
-
-scp -r .\packages\manage-client\dist\* root@119.29.246.22:/usr/software/manage/nginx/html
-scp .\packages\manage-client\nginx.conf root@119.29.246.22:/usr/software/manage/nginx/conf
